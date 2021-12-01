4662018cf95dd08234e15f2a98514039f6fa1a0d
Amine Bakkou <amine-bakkou@live.fr>
2021-12-01 23:28:36 +0100

    Adding Composite Logger

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -3,8 +3,8 @@ package fr.lernejo.logger;
 public class LoggerFactory {
 
     public static Logger getLogger(String name){
-        Logger logger = new FileLogger("./logs.txt");
-        return new ContextualLogger(name, logger);
+        Logger filelogger = new FileLogger("./logs.txt");
+        return new CompositeLogger(new ContextualLogger(name,new ConsoleLogger()), new ContextualLogger(name,filelogger));
 
     }
 }
